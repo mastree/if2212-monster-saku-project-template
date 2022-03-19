ebe727bed6f80d8ab277427114db3750efb220f8
Kamal S <muha.kamal@gmail.com>
2022-03-19 18:02:47 +0700

    minor change on main

--- src/main/java/com/monstersaku/Main.java
+++ src/main/java/com/monstersaku/Main.java
@@ -4,35 +4,31 @@ import com.monstersaku.util.CSVReader;
 
 import java.io.File;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.ArrayList;
 
 public class Main {
-    private static final List<String> CSV_FILE_PATHS = Arrays.asList(
+    private static final List<String> CSV_FILE_PATHS = Collections.unmodifiableList(Arrays.asList(
             "configs/monsterpool.csv",
             "configs/movepool.csv",
-            "configs/element-type-effectivity-chart.csv");
-    public static void main(String[] args) {
-        List<CSVReader> readers = new ArrayList<>();
+            "configs/element-type-effectivity-chart.csv"));
 
+    public static void main(String[] args) {
         for (String fileName : CSV_FILE_PATHS) {
             try {
-                readers.add(new CSVReader(new File(Main.class.getResource(fileName).toURI()), ";"));
-            } catch (Exception e) {
-                // do nothing
-            }
-        }
-
-        for (CSVReader reader : readers) {
-            reader.setSkipHeader(true);
-            try {
+                System.out.printf("Filename: %s\n", fileName);
+                CSVReader reader = new CSVReader(new File(Main.class.getResource(fileName).toURI()), ";");
+                reader.setSkipHeader(true);
                 List<String[]> lines = reader.read();
+                System.out.println("=========== CONTENT START ===========");
                 for (String[] line : lines) {
                     for (String word : line) {
                         System.out.printf("%s ", word);
                     }
                     System.out.println();
                 }
+                System.out.println("=========== CONTENT END ===========");
                 System.out.println();
             } catch (Exception e) {
                 // do nothing
